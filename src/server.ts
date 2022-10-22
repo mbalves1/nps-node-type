fc00e2e4c0e6189acaa22e72be97cd5c867353af
Murilo <mbalves1@outlook.com>
2022-10-22 10:51:47 -0300

    init project

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata";
+import express from 'express';
+import './database';
+import { router } from "./routes";
+
+const app = express();
+
+const port = 3333
+
+app.use(express.json());
+app.use(router);
+
+app.listen(port, () => console.log(`Server is running in port: ${port}!`));
