f8cd7d8ca281f77e19644cfdb8ded954e0dfd1df
Murilo <mbalves1@outlook.com>
2022-12-05 20:10:58 -0300

    [feat] - install yup validate

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,21 +1,35 @@
-import { Request, Response } from 'express';
+import { request, Request, response, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UserRepository';
+import * as yup from 'yup';
 
 class UserController {
-  async create(req: Request, res: Response) {
-    const { name, email } = req.body;
+  async create(request: Request, res: Response) {
+    const { name, email } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required('Nome obrigatório'),
+      email: yup.string().email().required('Email incorreto')
+    })
+    
+    try {
+      await schema.validate(request.body, { abortEarly: false })
+    } catch(err) {
+      return res.status(400).json({ error: err });
+    }
 
     const usersRepository = getCustomRepository(UsersRepository)
 
     const userAlreadyExists = await usersRepository.findOne({
       email
     })
+
     if(userAlreadyExists) {
       return res.status(400).json({
         error: "User already exists!"
       })
     }
+
     const user = usersRepository.create({
       name, email
     })
